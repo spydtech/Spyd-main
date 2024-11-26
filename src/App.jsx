65b19729c262fe27fce9dc506da309fe2d90ff9e
Prabhashyam <116556508+7893787849@users.noreply.github.com>
2024-11-26 12:36:08 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,58 @@ import ContentMarketing from "./components/digitalmarketing/contentmarketing/CmM
 import EmailMarketing from "./components/digitalmarketing/emailmarketing/EmMain";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AdvancedTech from './components/OtherTechnology/AdvancedTech'
-import Navbar from './components/Navbar'
-import CloudTech from './components/OtherTechnology/CloudTech/CloudTech'
-import Testing from './components/OtherTechnology/Testing/Testing'
-import AiandMl from './components/OtherTechnology/Ai&Ml/AiandMl'
-import Aws from './components/OtherTechnology/CloudTech/Aws'
+
+import Navbar from "./components/Navbar"
+import CloudAzure from "./components/OtherTech/CloudTech/CloudAzure";
+import GCloud from "./components/OtherTech/CloudTech/GCloud";
+
+
+
+
+import CloudTech from "./components/OtherTech/CloudTech/CloudTech";
+import Testing from "./components/OtherTech/Testing/Testing";
+import AiandMl from "./components/OtherTech/AiandML/AiandML";
+import Ai from "./components/OtherTech/AiandML/Ai";
+
+import MlLearning from "./components/OtherTech/AiandML/MlLearning";
+import Aws from "./components/OtherTech/CloudTech/Aws";
+
+import Carrers from "./components/carrers/CarrersMain";
+
+import AdvancedTech from "./components/OtherTech/AdvnacedTech";
+import Frontend from "./components/frontend/Frontend";
+import Angular from "./components/frontend/Angular";
+import NextJs from "./components/frontend/NextJs";
+import React from "./components/frontend/ReactJs";
+
+
+// Microsoft
+import ServicesHome from "./components/microsoft/services/ServicesHome";
+import Services from "./components/microsoft/CRM/Services";
+import PowerBi from "./components/microsoft/powerbi/PowerBi"
+
+
+// ERPNext
+import ERPNextConsultancy from "./components/ERPNext/ERPNextConsultancy";
+import ERPimpPage from "./components/ERPNext/ERPimpPage";
+import ERPfaq from "./components/ERPNext/ERPfaq";
+              
+
+import ERPServices from "./components/ERPNext/ERPServices";
+
+
+
+import AboutUs from "./components/company/AboutUs";
+import OurCoreValues from "./components/company/OurCoreValues";
+import VisionAndMission from "./components/company/VisionAndMission";
+import WhatMakesUsDifferent from "./components/company/WhatMakesUsDifferent";
+import StaffingServices from "./components/company/staffingServices/StaffingServices";
+
+import Backend from "./components/backend/BackendMain";
+import JavaMain from "./components/backend/java/JavaMain";
+import PhpMain from "./components/backend/php/PhpMain";
+import PythonMain from "./components/backend/python/PythonMain";
+
 
 function App() {
   return (
